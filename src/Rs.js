807f47f503f61324d5bbb20ffc8c5239588b2cb9
anukummari121 <93691280+anukummari121@users.noreply.github.com>
2022-03-31 13:37:39 -0400

    first commit

---
+++ src/Rs.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Rs = () => {
+  return (
+    <>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque pariatur
+      odio adipisci doloremque aspernatur quas molestias harum blanditiis minima
+      libero error, fugiat reprehenderit asperiores veniam? Eius qui commodi non
+      delectus.
+    </>
+  );
+};
+
+export default Rs;
